44f8cd474ad2d0b1671d86d17222238c566fb697
lciampiconi <lorenzo.ciampiconi@mail.polimit.it>
2022-09-10 18:14:47 +0200

    added system_requirements.txt and data setup

--- data_setup.sh
+++ data_setup.sh
@@ -1,5 +1,7 @@
+apt update && apt install -y `cat system_requirements.txt`
 kaggle competitions download -c open-problems-multimodal
 unzip open-problems-multimodal.zip
+rm open-problems-multimodal.zip
 mv *.h5 data/
 mv *.csv data/
 python3 data_handling.py
