7c4e60de2dd1d543bbea1f6a3ae65db50d32fe8b
Guido Ballabio <guido.ballabio@aramis3d.com>
2022-09-12 16:54:20 +0200

    improve gitignore and minor fixes

--- data_setup.sh
+++ data_setup.sh
@@ -1,6 +1,4 @@
+#!/bin/sh
 kaggle competitions download -c open-problems-multimodal
-unzip open-problems-multimodal.zip
-rm open-problems-multimodal.zip
-mv *.h5 data/
-mv *.csv data/
-python3 data_handling.py
+unzip open-problems-multimodal.zip -d data/
+python3 single_cell_multimodal_core/data_handling.py
