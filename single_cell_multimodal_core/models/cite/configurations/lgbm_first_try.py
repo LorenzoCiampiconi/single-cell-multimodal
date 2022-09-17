8892c880fa977b1f322d6916791437eee53d560a
lciampiconi <lorenzo.ciampiconi@mail.polimit.it>
2022-09-17 21:28:17 +0200

    lgbm first model configuration

--- single_cell_multimodal_core/models/cite/configurations/lgbm_first_try.py
+++ single_cell_multimodal_core/models/cite/configurations/lgbm_first_try.py
@@ -1,19 +1,30 @@
-model_label = 'lgbm_first_try'
+model_label = "lgbm_first_try"
 
-params = {
-     'learning_rate': 0.1,
-     'objective' : 'regression',
-     'metric': 'rmse',#mae',
-     'random_state': 4223,
-     'reg_alpha': 0.03,
-     'reg_lambda': 0.002,
-     'colsample_bytree': 0.8,
-     'subsample': 0.6,
-     'max_depth': 10,
-     'num_leaves': 186,
-     'min_child_samples': 263
-    }
+global_params = {
+    "seed": 0,
+}
 
-cross_validation_params = {
-     'n_splits_for_kfold':2
+model_params = {
+    "learning_rate": 0.1,
+    "objective": "regression",
+    "metric": "rmse",  # mae',
+    "random_state": 0,
+    "reg_alpha": 0.03,
+    "reg_lambda": 0.002,
+    "colsample_bytree": 0.8,
+    "subsample": 0.6,
+    "max_depth": 10,
+    "num_leaves": 186,
+    "min_child_samples": 263,
+}
+
+cross_validation_params = {"n_splits_for_kfold": 10}
+
+svd_params = {"n_components": 64}
+
+configuration = {
+    "cross_validation_params": cross_validation_params,
+    "model_params": model_params,
+    "svd_params": svd_params,
+     "global_params": global_params
 }
