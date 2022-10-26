f5c8513dae7c0cdb49ab2fec02c1b7843649e693
lciampiconi <lorenzo.ciampiconi@mail.polimit.it>
2022-10-26 11:55:13 +0200

    removed RF from __main__.py

--- scmm/models/cite/__main__.py
+++ scmm/models/cite/__main__.py
@@ -1,5 +1,3 @@
-'''
-MAIN LGBM + AUTOENCODER
 from scmm.models.cite.concrete import LGBMwMultilevelEmbedderCite
 from scmm.models.cite.configurations.lgbm_w_autoencoder_small import configuration, model_label
 from scmm.utils.log import setup_logging
@@ -9,13 +7,6 @@ if __name__ == "__main__":
     model_wrapper = LGBMwMultilevelEmbedderCite(configuration=configuration, label=model_label)
 
     model_wrapper.full_pipeline(save_model=True)
-'''
 
-from scmm.models.cite.concrete import RFCite
-from scmm.models.cite.configurations.rf import configuration,model_label
-from scmm.utils.log import setup_logging
 
-if __name__ == "__main__":
-    setup_logging("DEBUG")
-    model_wrapper = RFCite(configuration=configuration, label=model_label)
-    model_wrapper.full_pipeline(save_model=True)
+
