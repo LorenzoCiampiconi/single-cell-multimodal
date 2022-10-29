1514048b06310341cc4d8cdc20309a8a6f4945f8
Andrea Agostini <agostini95@gmail.com>
2022-10-29 10:13:15 +0200

    fix argparse main

--- scmm/main_tmp.py
+++ scmm/main_tmp.py
@@ -20,24 +20,25 @@ from scmm.utils.log import setup_logging
 from scmm.problems.cite.concrete import LGBMwMultilevelEmbedderCite
 from scmm.problems.cite.configurations.config_dict import config_dic
 
-# logging setup
-setup_logging("DEBUG")
-logger = logging.getLogger(__name__)
+if __name__ == "__main__":
+    # logging setup
+    setup_logging("DEBUG")
+    logger = logging.getLogger(__name__)
 
-# argparse setup
-parser = argparse.ArgumentParser(description='scmm full pipline')
-parser.add_argument("config_name", type=str, help='configuration name or configuration file name')
-args = parser.parse_args()
+    # argparse setup
+    parser = argparse.ArgumentParser(description='scmm full pipline')
+    parser.add_argument("config_name", type=str, help='configuration name or configuration file name')
+    args = parser.parse_args()
 
-if args.config_name in config_dic:
-    config_file = config_dic.get(args.config_name)
-else:
-    config_file = args.config_name
+    if args.config_name in config_dic:
+        config_file = config_dic.get(args.config_name)
+    else:
+        config_file = args.config_name
 
-config_module = importlib.import_module("scmm.problems.cite.configurations."+config_file)
-logger.info("starting full pipeline with configuration: " f"{config_module.model_label}")
+    config_module = importlib.import_module("scmm.problems.cite.configurations."+config_file)
+    logger.info("starting full pipeline with configuration: " f"{config_module.model_label}")
 
-model_wrapper = LGBMwMultilevelEmbedderCite(configuration=config_module.configuration, label=config_module.model_label)
-model_wrapper.full_pipeline()
+    model_wrapper = LGBMwMultilevelEmbedderCite(configuration=config_module.configuration, label=config_module.model_label)
+    model_wrapper.full_pipeline()
 
 
